8ae547d6842d809629292cdcec30f6d06457cffc
Mahdi Tasha <imwhdiiii@gmail.com>
2023-11-04 17:53:17 +0330

    added home page

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+// Importing part
+import {ReactNode} from "react";
+
+// Creating and exporting home page as default
+export default function HomePage():ReactNode {
+    // Returning JSX
+    return (
+        <section className={'min-h-screen flex justify-center items-center'}>
+            <main>
+                <h1 className={'lg:text-[30px] text-[20px] text-white text-center font-black tracking-widest'}>HOME PAGE</h1>
+            </main>
+        </section>
+    );
+}
