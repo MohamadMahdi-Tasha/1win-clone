73659a064212c8ba154b85dabab01d92ae53c218
Mahdi Tasha <imwhdiiii@gmail.com>
2023-11-05 16:30:38 +0330

    added scripts to close and open lang dropdown in top side of header

--- chunk/header/topside/langDropdownComponent/index.tsx
+++ chunk/header/topside/langDropdownComponent/index.tsx
@@ -1,5 +1,8 @@
+// Forcing NextJS to render this component as client side component
+'use client';
+
 // Importing part
-import {ReactNode} from "react";
+import {Dispatch, ReactNode, useState} from "react";
 import IconComponent from "@/chunk/iconComponent";
 import LangDropdownItemComponent from "@/chunk/header/topside/langDropdownComponent/langDropdownItemComponent";
 import EnImage from '@/public/img/header/flags/img-en.svg';
@@ -30,47 +33,54 @@ import ZaImage from '@/public/img/header/flags/img-za.svg';
 
 // Creating and exporting language dropdown as default
 export default function LangDropdownComponent():ReactNode {
+    // Defining state of component
+    const [isOpened, setOpened]:[boolean, Dispatch<boolean>] = useState(false);
+
     // Returning JSX
     return (
         <div className={'relative'}>
-            <button className={'h-[30px] px-[5px] bg-white/10 flex items-center rounded-[5px] gap-[10px]'}>
+            <button className={'h-[30px] px-[5px] bg-white/10 flex items-center rounded-[5px] gap-[10px]'}
+                    onClick={() => {(isOpened) ? setOpened(false) : setOpened(true)}}
+            >
                 <span className={'block text-[10px] text-white'}>EN</span>
                 <span className={'text-white rotate-180'}><IconComponent name={'chevron'} size={7} /></span>
                 <img className={'aspect-square w-[20px]'} src={EnImage.src} alt="Flag of england"/>
             </button>
-            <div className={'absolute top-[130%] right-0 bg-white/10 overflow-auto rounded-[20px] w-[300px] h-[500px] p-[20px] transition-all duration-500'}>
-                <span className={'block truncate text-white text-[15px] font-bold mb-[15px]'}>Suggested languages</span>
-                <ul className={'rounded-[20px] overflow-hidden'}>
-                    <LangDropdownItemComponent img={FrImage.src} name={'fr'} isSelected={false} />
-                    <LangDropdownItemComponent img={EnImage.src} name={'en'} isSelected={true} />
-                    <LangDropdownItemComponent img={RuImage.src} name={'ru'} isSelected={false} />
-                </ul>
-                <span className={'block truncate text-white text-[15px] font-bold my-[15px]'}>All languages</span>
-                <ul className={'rounded-[20px] overflow-hidden'}>
-                    <LangDropdownItemComponent img={AzImage.src} name={'az'} isSelected={false} />
-                    <LangDropdownItemComponent img={BdImage.src} name={'bd'} isSelected={false} />
-                    <LangDropdownItemComponent img={BrImage.src} name={'br'} isSelected={false} />
-                    <LangDropdownItemComponent img={CnImage.src} name={'cn'} isSelected={false} />
-                    <LangDropdownItemComponent img={DeImage.src} name={'de'} isSelected={false} />
-                    <LangDropdownItemComponent img={EsImage.src} name={'es'} isSelected={false} />
-                    <LangDropdownItemComponent img={GrImage.src} name={'gr'} isSelected={false} />
-                    <LangDropdownItemComponent img={IdImage.src} name={'id'} isSelected={false} />
-                    <LangDropdownItemComponent img={InImage.src} name={'in'} isSelected={false} />
-                    <LangDropdownItemComponent img={ItImage.src} name={'it'} isSelected={false} />
-                    <LangDropdownItemComponent img={JpImage.src} name={'jp'} isSelected={false} />
-                    <LangDropdownItemComponent img={KgImage.src} name={'kg'} isSelected={false} />
-                    <LangDropdownItemComponent img={KoImage.src} name={'ko'} isSelected={false} />
-                    <LangDropdownItemComponent img={KzImage.src} name={'kz'} isSelected={false} />
-                    <LangDropdownItemComponent img={MdImage.src} name={'md'} isSelected={false} />
-                    <LangDropdownItemComponent img={MxImage.src} name={'mx'} isSelected={false} />
-                    <LangDropdownItemComponent img={PlImage.src} name={'pl'} isSelected={false} />
-                    <LangDropdownItemComponent img={TjImage.src} name={'tj'} isSelected={false} />
-                    <LangDropdownItemComponent img={TrImage.src} name={'tr'} isSelected={false} />
-                    <LangDropdownItemComponent img={UaImage.src} name={'ua'} isSelected={false} />
-                    <LangDropdownItemComponent img={UzImage.src} name={'uz'} isSelected={false} />
-                    <LangDropdownItemComponent img={ZaImage.src} name={'za'} isSelected={false} />
-                    <LangDropdownItemComponent img={InImage.src} name={'marathi'} isSelected={false} />
-                </ul>
+            <div data-opened={isOpened} className={'absolute top-[130%] right-0 bg-white/10 overflow-hidden rounded-[20px] w-[300px] h-[500px] p-[20px] transition-all duration-500 data-[opened="false"]:opacity-0 data-[opened="true"]:opacity-100 data-[opened="false"]:invisible data-[opened="true"]:visible'}>
+                <div className={'overflow-auto w-full h-full pr-[10px] lang-dropdown'}>
+                    <span className={'block truncate text-white text-[15px] font-bold mb-[15px]'}>Suggested languages</span>
+                    <ul className={'rounded-[20px] overflow-hidden'}>
+                        <LangDropdownItemComponent img={FrImage.src} name={'fr'} isSelected={false} />
+                        <LangDropdownItemComponent img={EnImage.src} name={'en'} isSelected={true} />
+                        <LangDropdownItemComponent img={RuImage.src} name={'ru'} isSelected={false} />
+                    </ul>
+                    <span className={'block truncate text-white text-[15px] font-bold my-[15px]'}>All languages</span>
+                    <ul className={'rounded-[20px] overflow-hidden'}>
+                        <LangDropdownItemComponent img={AzImage.src} name={'az'} isSelected={false} />
+                        <LangDropdownItemComponent img={BdImage.src} name={'bd'} isSelected={false} />
+                        <LangDropdownItemComponent img={BrImage.src} name={'br'} isSelected={false} />
+                        <LangDropdownItemComponent img={CnImage.src} name={'cn'} isSelected={false} />
+                        <LangDropdownItemComponent img={DeImage.src} name={'de'} isSelected={false} />
+                        <LangDropdownItemComponent img={EsImage.src} name={'es'} isSelected={false} />
+                        <LangDropdownItemComponent img={GrImage.src} name={'gr'} isSelected={false} />
+                        <LangDropdownItemComponent img={IdImage.src} name={'id'} isSelected={false} />
+                        <LangDropdownItemComponent img={InImage.src} name={'in'} isSelected={false} />
+                        <LangDropdownItemComponent img={ItImage.src} name={'it'} isSelected={false} />
+                        <LangDropdownItemComponent img={JpImage.src} name={'jp'} isSelected={false} />
+                        <LangDropdownItemComponent img={KgImage.src} name={'kg'} isSelected={false} />
+                        <LangDropdownItemComponent img={KoImage.src} name={'ko'} isSelected={false} />
+                        <LangDropdownItemComponent img={KzImage.src} name={'kz'} isSelected={false} />
+                        <LangDropdownItemComponent img={MdImage.src} name={'md'} isSelected={false} />
+                        <LangDropdownItemComponent img={MxImage.src} name={'mx'} isSelected={false} />
+                        <LangDropdownItemComponent img={PlImage.src} name={'pl'} isSelected={false} />
+                        <LangDropdownItemComponent img={TjImage.src} name={'tj'} isSelected={false} />
+                        <LangDropdownItemComponent img={TrImage.src} name={'tr'} isSelected={false} />
+                        <LangDropdownItemComponent img={UaImage.src} name={'ua'} isSelected={false} />
+                        <LangDropdownItemComponent img={UzImage.src} name={'uz'} isSelected={false} />
+                        <LangDropdownItemComponent img={ZaImage.src} name={'za'} isSelected={false} />
+                        <LangDropdownItemComponent img={InImage.src} name={'marathi'} isSelected={false} />
+                    </ul>
+                </div>
             </div>
         </div>
     );
