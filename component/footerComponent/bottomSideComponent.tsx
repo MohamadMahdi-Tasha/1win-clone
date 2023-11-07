74d7c9843f0f7fde839561fa022fc65b1b54a5a0
Mahdi Tasha <imwhdiiii@gmail.com>
2023-11-07 17:24:10 +0330

    added styles of bottom side in footer

--- component/footerComponent/bottomSideComponent.tsx
+++ component/footerComponent/bottomSideComponent.tsx
@@ -12,17 +12,24 @@ import RatingImage from '@/public/img/footer/bottom/img-rating.svg';
 export default function BottomSideComponent():ReactNode {
     // Returning JSX
     return (
-        <div>
-            <span>© 2023 1WIN.</span>
-            <div>
-                <img src={BestImage.src} alt="Best Image"/>
-                <img src={BetrajaImage.src} alt="Betraja Image"/>
-                <img src={BettingImage.src} alt="Betting Image"/>
-                <img src={CasinoImage.src} alt="Casino Image"/>
-                <img src={CasinoMentorImage.src} alt="CasinoMentor Image"/>
-                <img src={CricketImage.src} alt="Cricket Image"/>
-                <img src={RatingImage.src} alt="Rating Image"/>
-                <span>18+</span>
+        <div className={'flex justify-between items-center py-[20px]'}>
+            <span className={'text-white/50 text-[12px]'}>© 2023 1WIN.</span>
+            <div className={'flex items-center gap-[20px]'}>
+                <a href="#"><img className={'w-[50px]'} src={BestImage.src} alt="Best Image"/></a>
+                <div className={'h-[20px] w-[1px] bg-white/10'} />
+                <a href="#"><img className={'w-[50px]'} src={BetrajaImage.src} alt="Betraja Image"/></a>
+                <div className={'h-[20px] w-[1px] bg-white/10'} />
+                <a href="#"><img className={'w-[50px]'} src={BettingImage.src} alt="Betting Image"/></a>
+                <div className={'h-[20px] w-[1px] bg-white/10'} />
+                <a href="#"><img className={'w-[50px]'} src={CasinoImage.src} alt="Casino Image"/></a>
+                <div className={'h-[20px] w-[1px] bg-white/10'} />
+                <a href="#"><img className={'w-[50px]'} src={CasinoMentorImage.src} alt="CasinoMentor Image"/></a>
+                <div className={'h-[20px] w-[1px] bg-white/10'} />
+                <a href="#"><img className={'w-[50px]'} src={CricketImage.src} alt="Cricket Image"/></a>
+                <div className={'h-[20px] w-[1px] bg-white/10'} />
+                <a href="#"><img className={'w-[50px]'} src={RatingImage.src} alt="Rating Image"/></a>
+                <div className={'h-[20px] w-[1px] bg-white/10'} />
+                <span className={'text-[25px] text-white/50 font-bold'}>18+</span>
             </div>
         </div>
     );
