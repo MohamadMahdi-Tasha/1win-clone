6efc6518f693dc0afe184366f887131a6d31d4bb
Mahdi Tasha <imwhdiiii@gmail.com>
2023-11-10 11:00:03 +0330

    added active style of recent wins component

--- chunk/page/casino/recentWinsComponent.tsx
+++ chunk/page/casino/recentWinsComponent.tsx
@@ -9,14 +9,15 @@ interface propsType {
     gameTitle: string;
     email: string;
     link: string;
+    isActive: boolean;
 }
 
 // Creating and exporting recent wins component as default
-export default function RecentWinsComponent({email, gameTitle, price, img, link}:propsType):ReactNode {
+export default function RecentWinsComponent({email, gameTitle, price, img, link, isActive}:propsType):ReactNode {
     // Returning JSX
     return (
         <li>
-            <Link href={link} className={'w-[120px] bg-lightTheme p-[5px] rounded-[10px] block'}>
+            <Link data-active={isActive} href={link} className={'w-[120px] p-[5px] rounded-[10px] block data-[active="false"]:bg-lightTheme data-[active="true"]:bg-gradient-to-b from-orange-500 to-pink-700'}>
                 <img className={'h-[75px] w-full object-cover rounded-[10px] mb-[5px]'} src={img} alt="Game image"/>
                 <div className={'w-full overflow-hidden'}>
                     <span className={'block text-white truncate text-[12px]'}>{price.toLocaleString()}$</span>
