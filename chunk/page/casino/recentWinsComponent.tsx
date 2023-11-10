37a7f1cff507e25565063cbc8a3d0f297c894acd
Mahdi Tasha <imwhdiiii@gmail.com>
2023-11-10 10:45:40 +0330

    added a link to recent wins comonent

--- chunk/page/casino/recentWinsComponent.tsx
+++ chunk/page/casino/recentWinsComponent.tsx
@@ -1,5 +1,6 @@
 // Importing part
 import {ReactNode} from "react";
+import Link from "next/link";
 
 // Defining type of props
 interface propsType {
@@ -7,19 +8,22 @@ interface propsType {
     price: number;
     gameTitle: string;
     email: string;
+    link: string;
 }
 
 // Creating and exporting recent wins component as default
-export default function RecentWinsComponent({email, gameTitle, price, img}:propsType):ReactNode {
+export default function RecentWinsComponent({email, gameTitle, price, img, link}:propsType):ReactNode {
     // Returning JSX
     return (
-        <li className={'w-[120px] bg-lightTheme p-[5px] rounded-[10px]'}>
-            <img className={'h-[75px] w-full object-cover rounded-[10px] mb-[5px]'} src={img} alt="Game image"/>
-            <div className={'w-full overflow-hidden'}>
-                <span className={'block text-white truncate text-[12px]'}>{price.toLocaleString()}$</span>
-                <span className={'block text-white truncate text-[10px]'}>{gameTitle}</span>
-                <span className={'block text-white/70 truncate text-[10px]'}>{email}</span>
-            </div>
+        <li>
+            <Link href={link} className={'w-[120px] bg-lightTheme p-[5px] rounded-[10px] block'}>
+                <img className={'h-[75px] w-full object-cover rounded-[10px] mb-[5px]'} src={img} alt="Game image"/>
+                <div className={'w-full overflow-hidden'}>
+                    <span className={'block text-white truncate text-[12px]'}>{price.toLocaleString()}$</span>
+                    <span className={'block text-white truncate text-[10px]'}>{gameTitle}</span>
+                    <span className={'block text-white/70 truncate text-[10px]'}>{email}</span>
+                </div>
+            </Link>
         </li>
     );
 }
